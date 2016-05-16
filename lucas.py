093bb8ae6e02ca2783a88baa34378113ea24003e
lucasperanzini <lucassperanzini4@gmail.com>
2016-05-16 17:30:06 -0300

    calculadora de dias vividos

---
+++ lucas.py
@@ -0,0 +1,7 @@
+dia = input("Que dia voce nasceu?")
+mes = input ("Qual mes voce nasceu?")
+ano = input ("Qual ano voce nasceu?")
+
+resultado = (2016 - ano)*365 + (5 - mes)*30 + 16 - dia
+
+print resultado
